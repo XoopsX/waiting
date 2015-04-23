793073855275f038b5d1d6bcec56db2a1c2ac114
nao-pon <hypweb@gmail.com>
2015-04-23 15:20:55 +0900

    waiting version 0.96

--- html/modules/waiting/xoops_version.php
+++ html/modules/waiting/xoops_version.php
@@ -30,7 +30,7 @@
 // ------------------------------------------------------------------------- //
 
 $modversion['name'] = _MI_WAITING_NAME;
-$modversion['version'] = 0.95;
+$modversion['version'] = 0.96;
 $modversion['description'] = _MI_WAITING_DESC;
 $modversion['author'] = "";
 $modversion['credits'] = "Ryus";
